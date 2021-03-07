594d6f2aafec62854f42f087828262c9a8feb858
Cayce Williams <caycew@umich.edu>
2021-03-06 19:42:35 -0600

    Update argument parser

--- index.js
+++ index.js
@@ -32,10 +32,21 @@ parser.add_argument('-d', '--dist', { help: 'The final dist/build folder path', 
 parser.add_argument('-a', '--assets', { help: 'The assets folder path', default: PATH_CONFIG.ASSETS });
 parser.add_argument('-c', '--css', { help: 'The css folder path', default: PATH_CONFIG.CSS });
 parser.add_argument('-j', '--js', { help: 'The js folder path', default: PATH_CONFIG.JS });
-parser.add_argument('-tsj', '--tag-start-js', { help: 'The js start tag', default: TAG_CONFIG.JS.START });
-parser.add_argument('-tej', '--tag-end-js', { help: 'The js end tag', default: TAG_CONFIG.JS.START });
 parser.add_argument('-tsc', '--tag-start-css', { help: 'The css start tag', default: TAG_CONFIG.CSS.START });
-parser.add_argument('-tec', '--tag-end-css', { help: 'The css end tag', default: TAG_CONFIG.CSS.START });
- 
-const bundler = new Bundler(PATH_CONFIG, TAG_CONFIG);
-bundler.create();
+parser.add_argument('-tec', '--tag-end-css', { help: 'The css end tag', default: TAG_CONFIG.CSS.END });
+parser.add_argument('-tsj', '--tag-start-js', { help: 'The js start tag', default: TAG_CONFIG.JS.START });
+parser.add_argument('-tej', '--tag-end-js', { help: 'The js end tag', default: TAG_CONFIG.JS.END });
+
+const args = parser.parse_args();
+console.log(args);
+if (args.html) PATH_CONFIG.HTML = args.html;
+if (args.dist) PATH_CONFIG.DIST = args.dist;
+if (args.assets) PATH_CONFIG.ASSETS = args.assets;
+if (args.css) PATH_CONFIG.CSS = args.css;
+if (args.js) PATH_CONFIG.JS = args.js;
+if (args.tag_start_css) TAG_CONFIG.CSS.START = args.tag_start_css;
+if (args.tag_end_css) TAG_CONFIG.CSS.END = args.tag_end_css;
+if (args.tag_start_js) TAG_CONFIG.JS.START = args.tag_start_js;
+if (args.tag_end_js) TAG_CONFIG.JS.END = args.tag_end_js;
+
+new Bundler(PATH_CONFIG, TAG_CONFIG).create();
